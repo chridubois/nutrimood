fa0c7e19b035452ef3fe4c26ed6ce5307bf39e3d
Christophe Dubois <idategto11@gmail.com>
2023-03-07 17:30:01 +0100

    Ajout des seeds ingredients, recettes

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,9 @@ require 'csv'
 Mood.destroy_all
 Symptom.destroy_all
 User.destroy_all
+RecipesIngredient.destroy_all
+RecipesStep.destroy_all
+Ingredient.destroy_all
 Recipe.destroy_all
 
 # Création des moods
@@ -49,49 +52,72 @@ html_doc = Nokogiri::HTML.parse(html_file)
 #Get all recipes
 recipe_urls = html_doc.xpath('//article//a/@href').map {|attr| attr.value}
 
-# For each recipe, get recipe infos
-html_file = URI.open('https://recettehealthy.com/soupe-de-legumes-poulet-cremeuse/').read
-html_doc = Nokogiri::HTML.parse(html_file)
+recipe_urls.each do |url|
+  # For each recipe, get recipe infos
+  html_file = URI.open(url).read
+  html_doc = Nokogiri::HTML.parse(html_file)
 
-# Get Recipe name
-name = html_doc.search('.wprm-recipe-container h2').text
+  # Get Recipe name
+  name = html_doc.search('.wprm-recipe-container h2').text
 
-# Get Recipe prep_time
-prep_time = html_doc.search('.wprm-recipe-details.wprm-recipe-details-minutes.wprm-recipe-prep_time.wprm-recipe-prep_time-minutes').text
+  # Get Recipe prep_time
+  prep_time = html_doc.search('.wprm-recipe-details.wprm-recipe-details-minutes.wprm-recipe-prep_time.wprm-recipe-prep_time-minutes').text.to_i
 
-# Get Recipe cook_time
-cook_time = html_doc.search('.wprm-recipe-details.wprm-recipe-details-minutes.wprm-recipe-cook_time.wprm-recipe-cook_time-minutes').text
+  # Get Recipe cook_time
+  cook_time = html_doc.search('.wprm-recipe-details.wprm-recipe-details-minutes.wprm-recipe-cook_time.wprm-recipe-cook_time-minutes').text.to_i
 
-# Get Recipe calories
-calories = html_doc.search('.wprm-recipe-details.wprm-recipe-nutrition.wprm-recipe-calories.wprm-block-text-normal').text
+  # Get Recipe calories
+  calories = html_doc.search('.wprm-recipe-details.wprm-recipe-nutrition.wprm-recipe-calories.wprm-block-text-normal').text.to_i
 
-# First creation of Recipe
-recipe = Recipe.create({
-  name: name,
-  calories_by_person: calories,
-  duration: prep_time + cook_time
-})
+  # Get Recipe image
+  image = html_doc.search('.wprm-recipe-image img').attr('data-lazy-src').text
 
+  # Get Recipe count of people
+  count_people = html_doc.search('.wprm-recipe-servings').text.to_i
 
-# Get Recipe Ingredients
-ingredients = html_doc.search('li.wprm-recipe-ingredient')
-p ingredients
-p '------------'
-i = 0
-ingredients.each do |ingredient|
-  quantity = ingredient.xpath('//span[@class="wprm-recipe-ingredient-amount"]').collect(&:text)[i]
-  unit = ingredient.xpath('//span[@class="wprm-recipe-ingredient-unit"]').collect(&:text)[i]
-  name = ingredient.xpath('//span[@class="wprm-recipe-ingredient-name"]').collect(&:text)[i]
-  p "#{quantity} - #{unit} - #{name}"
-  p '------------'
-i += 1
-end
+  # First creation of Recipe
+  recipe = Recipe.create({
+    name: name,
+    calories_by_person: calories,
+    duration: prep_time + cook_time,
+    image: image
+  })
 
-# Get Recipe steps
-steps = html_doc.search('li.wprm-recipe-instruction')
-i = 0
-steps.each do |step|
-  p step.content
-  p '------------'
-i += 1
+  # Get Recipe Ingredients
+  ingredients = html_doc.search('li.wprm-recipe-ingredient')
+  i = 0
+  ingredients.each do |ingredient|
+    quantity = ingredient.xpath('//span[@class="wprm-recipe-ingredient-amount"]').collect(&:text)[i].to_i
+    quantity_calc = (quantity / count_people.to_f).ceil(2)
+    unit = ingredient.xpath('//span[@class="wprm-recipe-ingredient-unit"]').collect(&:text)[i]
+    name = ingredient.xpath('//span[@class="wprm-recipe-ingredient-name"]').collect(&:text)[i]
+
+    if Ingredient.find_by_name(name).nil?
+      ingredient = Ingredient.create({
+        name: name
+      })
+    else
+      ingredient = Ingredient.find_by_name(name)
+    end
+
+    RecipesIngredient.create({
+      recipe: recipe,
+      ingredient: ingredient,
+      unit: unit,
+      quantity: quantity_calc
+    })
+  i += 1
+  end
+
+  # Get Recipe steps
+  steps = html_doc.search('li.wprm-recipe-instruction')
+  i = 0
+  steps.each do |step|
+    RecipesStep.create({
+      recipe: recipe,
+      step_number: i + 1,
+      step_description: step.text
+    })
+  i += 1
+  end
 end
