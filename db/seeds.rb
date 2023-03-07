a71d52e810130d10edbd985c0127807ae0068a3c
Christophe Dubois <idategto11@gmail.com>
2023-03-07 12:16:15 +0100

    début des seeds, création de moods et symptomes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'csv'
+
+# Création des moods
+CSV.foreach(Rails.root.join('lib/moods.csv'), headers: false) do |row|
+  p "Creating mood #{row[0]}"
+  Mood.create({
+    name: row[0]
+  })
+end
+
+# Création des symptomes
+CSV.foreach(Rails.root.join('lib/symptoms.csv'), headers: false) do |row|
+  p "Creating mood #{row[0]}"
+  Symptom.create({
+    name: row[0]
+  })
+end
