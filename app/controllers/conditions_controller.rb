06f834921ff5344990e43f413f03bbbec79ec3b4
exec zsh <candice.fays@ymail.com>
2023-03-08 14:41:07 +0100

    question3 devenue 4 en cours

--- app/controllers/conditions_controller.rb
+++ app/controllers/conditions_controller.rb
@@ -11,13 +11,14 @@ class ConditionsController < ApplicationController
     @condition = Condition.create(mood_id: @mood_id, user: current_user)
   end
 
-  def question3
+  def question4
+    @symptoms = Symptom.all
 # récupère l'instance de @condition crée en Q2
-    @condition = Condition.find(params[:id]) # ??? à voir fin Q2
+    # @condition = Condition.find(params[:id]) # ??? à voir fin Q2
 # crée une instance de la table de jointure symptoms_by_condition
-    @symptoms_association = NomTableJ.new
+    # @symptoms_association = NomTableJ.new
 # récup les @symptoms = id_symptoms du formulaire
-    @symptoms = []
+    # @symptoms = []
 # pour chaque ID_symptom récup:
 #      jointure asso ID
 # upload @condition
