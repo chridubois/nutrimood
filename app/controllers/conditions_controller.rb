47115f04d689d2cfabd2f3d43fc1c4f5b550f644
benedetti charlottebene@gmail.com <charlottebene@gmail.com>
2023-03-08 14:29:36 +0100

    question2

--- app/controllers/conditions_controller.rb
+++ app/controllers/conditions_controller.rb
@@ -1,23 +1,18 @@
 class ConditionsController < ApplicationController
 
   def question1
-<<<<<<< HEAD
     @moods = Mood.all
-=======
-    @condition = Condition.new
-    @moods = Mood.all
-
->>>>>>> 318e5fe227525a517d403359883da2e29a87d59c
   end
 
   def question2
     @mood_id = params[:mood]
     @condition = Condition.create(mood_id: @mood_id, user: current_user)
-    redirect_to question3_path
   end
 
   def question3
-
+    @condition = Condition.find_by(user: current_user)
+    @condition.energy_level = params[:energy_level]
+    @condition.update(energy_level: params[:energy_level])
   end
 
 end
