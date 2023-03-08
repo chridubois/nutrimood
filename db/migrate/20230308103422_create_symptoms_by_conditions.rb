dad501ff2188b3d210b8d9eea3aaec53d5bcccfb
Christophe Dubois <idategto11@gmail.com>
2023-03-08 12:00:28 +0100

    Ajout des seeds de symptoms by condition

---
+++ db/migrate/20230308103422_create_symptoms_by_conditions.rb
@@ -0,0 +1,10 @@
+class CreateSymptomsByConditions < ActiveRecord::Migration[7.0]
+  def change
+    create_table :symptoms_by_conditions do |t|
+      t.references :condition, null: false, foreign_key: true
+      t.references :symptom, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ test/models/symptoms_by_condition_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class SymptomsByConditionTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
