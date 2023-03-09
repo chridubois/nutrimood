805e28a4e0fa02faa9cd27ce35c308d0558538b5
benedetti charlottebene@gmail.com <charlottebene@gmail.com>
2023-03-09 17:16:43 +0100

    change

--- app/controllers/infos_controller.rb
+++ app/controllers/infos_controller.rb
@@ -3,17 +3,6 @@ class InfosController < ApplicationController
   def show
     @condition = Condition.find_by(user: current_user)
     @recipe = Recipe.find(params[:id])
-
-    @recipes3 = []
-
-    while @recipes3.size == 3
-      unless @recipes3.include?(ingredients_by_symptoms)
-        @recipes3 << @recipe.ingredients_by_symptoms
-      end
-    end
-    # 3.times do
-    # @recipes3 << @recipe.ingredients_by_symptoms
-    # end
   end
 
 end
