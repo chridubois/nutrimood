4f73bdf3e65f0fd2e9e66e2ddf604a0a5d44c349
exec zsh <candice.fays@ymail.com>
2023-03-13 17:40:09 +0100

    tous les stimulus ont pété attention pas de merge!!!

--- app/javascript/controllers/moods_list_controller.js
+++ app/javascript/controllers/moods_list_controller.js
@@ -2,13 +2,19 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="moods-list"
 export default class extends Controller {
-  static targets = ["button"]
+  static targets = ["btn", "item1"]
 
   connect() {
-    console.log('coucou stimulus !')
+    console.log('coucou c est stimulus !')
   }
-  buttonAppear() {
-    console.log(this.buttonTarget)
-    this.buttonTarget.removeAttribute("hidden")
+
+  btnAppear() {
+    console.log('submit appear')
+    this.btnTarget.removeAttribute("hidden")
+  }
+
+  selectAMood() {
+    console.log('select a mood and only one')
+    console.log(this.item1Targets)
   }
 }
