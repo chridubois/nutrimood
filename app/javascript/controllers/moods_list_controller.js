6cbf4287d11ce15004ee9f474d3a43b6308b6333
exec zsh <candice.fays@ymail.com>
2023-03-16 12:32:17 +0100

    modif js bug

--- app/javascript/controllers/moods_list_controller.js
+++ app/javascript/controllers/moods_list_controller.js
@@ -2,28 +2,32 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="moods-list"
 export default class extends Controller {
-  static targets = ["btn", "item1", "checkbox", "emoticone"]
+  static targets = ["btn", "item1", "input", "emoticone"]
   static values = {
-    url: String
+    moodId: {
+      type: Number, default: 0
+    },
+    urls: Array
   }
 
   connect() {
     console.log('coucou c est stimulus !')
-    console.log(this.urlValue);
+    console.log(this.urlsValue);
   }
 
   selectAMood(event) {
-    const image = document.querySelector("#emoticone");
-    const url = event.params["url"];
-    this.btnTarget.removeAttribute("hidden")
-    image.src = url;
-    image.removeAttribute("hidden")
-    if (this.checkboxTargets[event.params["idx"]].checked) {
-      this.item1Targets[event.params["idx"]].classList.add("selected")
-    }
-    else {
-     this.item1Targets[event.params["idx"]].classList.remove("selected")
-    }
+    this.moodIdValue = event.params.idx;
+  }
 
+  moodIdValueChanged(value, previousValue) {
+    if (this.moodIdValue) {
+      const url = this.urlsValue.filter((m) => m.id === value)[0].url;
+      this.emoticoneTarget.src = url;
+      this.emoticoneTarget.removeAttribute("hidden")
+      this.btnTarget.removeAttribute("hidden")
+      previousValue && this.item1Targets.filter((m) => Number(m.dataset.moodsListIdxParam) === previousValue)[0].classList.remove('selected')
+      value && this.item1Targets.filter((m) => Number(m.dataset.moodsListIdxParam) === value)[0].classList.add('selected');
+      this.inputTarget.value = value;
+    }
   }
 }
