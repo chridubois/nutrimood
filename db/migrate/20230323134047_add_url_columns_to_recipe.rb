99c57bfe73608ff99b6250fd39d3ba3031bc4b8d
Christophe Dubois <idategto11@gmail.com>
2023-03-24 11:03:50 +0100

    change last migration

--- db/migrate/20230323134047_add_url_columns_to_recipe.rb
+++ db/migrate/20230323134047_add_url_columns_to_recipe.rb
@@ -1,6 +1,6 @@
 class AddUrlColumnsToRecipe < ActiveRecord::Migration[7.0]
   def change
-    add_column :recipes, :website_source, :string, null: false
-    add_column :recipes, :website_url, :string, null: false
+    add_column :recipes, :website_source, :string
+    add_column :recipes, :website_url, :string
   end
 end
